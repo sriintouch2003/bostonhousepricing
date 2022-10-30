7268ec9a22ae6f420daf2b24ade5767c977fa2bb
Sri <sriintouch2003@gmail.com>
2022-10-29 23:06:31 -0500

    This commit added web screen to enter values and get prediccted score

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 import pickle
 from flask import Flask, request, app, jsonify, url_for, render_template
 
@@ -22,8 +23,18 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = model.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The house price pred is {}".format(output)) 
+
+
 if __name__=="__main__":
     app.run(debug=True)
+    
 
 
 
